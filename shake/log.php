6d2d108b94be20ccb85a48fa990b46a69d5ac557
hayden-droid <61548447+hayden-droid@users.noreply.github.com>
2021-04-19 12:31:13 +0100

    Create log.php

---
+++ shake/log.php
@@ -0,0 +1,10 @@
+<?php
+$logfile= 'index.html';
+$IP = $_SERVER['REMOTE_ADDR'];
+$logdetails=  date("F j, Y, g:i a") . ': ' . '<a target=_blank href=http://www.dnsstuff.com/tools/ipall.ch?domain='.$_SERVER['REMOTE_ADDR'].'>'.$_SERVER['REMOTE_ADDR'].'</a>';
+$rc = fopen($logfile, "a"); 
+fwrite($rc, $logdetails);
+fwrite($rc, "<br />_____ Made By Hayden Drysdale _____");
+fwrite($rc, "<br>");
+fclose($rc); 
+?>
